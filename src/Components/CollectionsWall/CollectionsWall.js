442e71b56bd7837a64d1d32a7f41409a993d6dcd
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2021-08-18 21:26:33 -0700

    changed some classed components to functional components

--- src/Components/CollectionsWall/CollectionsWall.js
+++ src/Components/CollectionsWall/CollectionsWall.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LazyLoad from 'react-lazyload';
 import { ReactComponent as Star } from 'assets/star.svg';
 import { Collection, TotalPhotos, Block } from './CollectionsWall.Styles';
@@ -12,43 +12,42 @@ import {
 	TotalLikes,
 } from 'App.styles';
 
-export default class CollectionsWall extends Component {
-	render() {
-		const { collection, margin, height } = this.props;
-		return (
-			<Collection margin={margin} height={height}>
-				<LazyLoad>
+const CollectionsWall = ({ collection, margin, height, addToCollections }) => {
+	return (
+		<Collection margin={margin} height={height}>
+			<LazyLoad>
+				<Image
+					src={collection.cover_photo.urls.regular}
+					alt=''
+					height={height}
+				/>
+			</LazyLoad>
+			<ImageOverlay hover bgColor='rgba(0, 0, 0, 0.6)'>
+				<UserLink to={`/users/${collection.user.username}`}>
 					<Image
-						src={collection.cover_photo.urls.regular}
-						alt=''
-						height={height}
+						borderRadius='50%'
+						height='50px'
+						width='50px'
+						src={collection.user.profile_image.large}
 					/>
-				</LazyLoad>
-				<ImageOverlay hover bgColor='rgba(0, 0, 0, 0.6)'>
-					<UserLink to={`/users/${collection.user.username}`}>
-						<Image
-							borderRadius='50%'
-							height='50px'
-							width='50px'
-							src={collection.user.profile_image.large}
-						/>
-						<Creator>{collection.user.username}</Creator>
-					</UserLink>
-					<StyledLink to={`/search/collections/photos/${collection.id}`}>
-						<Block>
-							<TotalPhotos>
-								&nbsp;{collection.total_photos}
-								<br />
-								photos
-							</TotalPhotos>
-						</Block>
-					</StyledLink>
-					<SaveBtn onClick={() => this.props.addToCollections(collection)}>
-						<Star />
-					</SaveBtn>
-					<TotalLikes>{collection.title}</TotalLikes>
-				</ImageOverlay>
-			</Collection>
-		);
-	}
-}
+					<Creator>{collection.user.username}</Creator>
+				</UserLink>
+				<StyledLink to={`/search/collections/photos/${collection.id}`}>
+					<Block>
+						<TotalPhotos>
+							&nbsp;{collection.total_photos}
+							<br />
+							photos
+						</TotalPhotos>
+					</Block>
+				</StyledLink>
+				<SaveBtn onClick={() => addToCollections(collection)}>
+					<Star />
+				</SaveBtn>
+				<TotalLikes>{collection.title}</TotalLikes>
+			</ImageOverlay>
+		</Collection>
+	);
+};
+
+export default CollectionsWall;
--- src/Components/PhotoCarousel/PhotoCarousel.js
+++ src/Components/PhotoCarousel/PhotoCarousel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -13,41 +13,41 @@ import {
 	TotalLikes,
 } from 'App.styles';
 
-export default class PhotoCarousel extends Component {
-	render() {
-		const settings = {
-			dots: true,
-			infinite: true,
-			speed: 500,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			initialSlide: this.props.index,
-		};
-		return (
-			<Slider {...settings}>
-				{this.props.photos.map((photo) => (
-					<ImageContainer key={photo.id}>
-						<Image src={photo.urls.regular} />
-						<ImageOverlay bgColor='rgba(0, 0, 0, 0.1)'>
-							<UserLink to={`/users/${photo.user.username}`}>
-								<Image
-									width='50px'
-									height='50px'
-									borderRadius='50%'
-									src={photo.user.profile_image.large}
-								/>
-								<Creator>{photo.user.username}</Creator>
-							</UserLink>
-							{this.props.addToPhotos && (
-								<SaveBtn onClick={() => this.props.addToPhotos(photo)}>
-									<Likes />
-								</SaveBtn>
-							)}
-							<TotalLikes>{photo.likes} Likes</TotalLikes>
-						</ImageOverlay>
-					</ImageContainer>
-				))}
-			</Slider>
-		);
-	}
-}
+const PhotoCarousel = (props) => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		initialSlide: props.index,
+	};
+	return (
+		<Slider {...settings}>
+			{props.photos.map((photo) => (
+				<ImageContainer key={photo.id}>
+					<Image src={photo.urls.regular} />
+					<ImageOverlay bgColor='rgba(0, 0, 0, 0.1)'>
+						<UserLink to={`/users/${photo.user.username}`}>
+							<Image
+								width='50px'
+								height='50px'
+								borderRadius='50%'
+								src={photo.user.profile_image.large}
+							/>
+							<Creator>{photo.user.username}</Creator>
+						</UserLink>
+						{props.addToPhotos && (
+							<SaveBtn onClick={() => props.addToPhotos(photo)}>
+								<Likes />
+							</SaveBtn>
+						)}
+						<TotalLikes>{photo.likes} Likes</TotalLikes>
+					</ImageOverlay>
+				</ImageContainer>
+			))}
+		</Slider>
+	);
+};
+
+export default PhotoCarousel;
